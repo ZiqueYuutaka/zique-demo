e234565e19bc5f2148d59a5b4ebe16158e40aebf
ZiqueYuutaka <izferren@gmail.com>
2019-06-23 23:09:34 -0400

    added spring boot app initializer

---
+++ src/main/java/com/zique/demo/config/MyDispatcherServlet.java
@@ -0,0 +1,9 @@
+package com.zique.demo.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MyDispatcherServlet extends SpringBootServletInitializer {
+
+}
