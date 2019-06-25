0df10e23d44035d1408fcf90eee9dee33869c465
ZiqueYuutaka <izferren@gmail.com>
2019-06-24 22:18:52 -0400

    removed dispatcher servlet

--- src/main/java/com/zique/demo/config/MyDispatcherServlet.java
+++
@@ -1,9 +0,0 @@
-package com.zique.demo.config;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class MyDispatcherServlet extends SpringBootServletInitializer {
-
-}
